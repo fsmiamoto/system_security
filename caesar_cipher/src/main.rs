b4ce80189d74d74aed2b0ff50c492fd21a2e0b99
Francisco Miamoto <fsmiamoto@gmail.com>
2021-06-24 21:17:55 -0300

    feat(caesar_cipher): add cli args parsing

--- caesar_cipher/src/main.rs
+++ caesar_cipher/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name="caesar", about = "A simple Caesar Cipher")]
+struct Args {
+    #[structopt(short,long)]
+    cipher: bool,
+    #[structopt(short,long)]
+    decipher: bool,
+    #[structopt(short = "k", long ="key")]
+    key: i8
 }
+
+fn main() {
+    let args = Args::from_args();
+    println!("{:?}", args);
+}
+
